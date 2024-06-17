957abbe67cd0ff5447ed53bb573f1e55d1a0961f
Martbul <martbul01@gmail.com>
2024-06-17 22:49:50 +0300

    working on OCR2

--- client/services/langchainServices.js
+++ client/services/langchainServices.js
@@ -1,27 +1,28 @@
 import { baseUrl, postRequest, getRequest } from "../utils/request";
 //TODO: za da bachka importa trqbva da e v survura!!!
-import { client } from "@gradio/client";
-export const askAQuestion = async (images, setAILearning,title,creator) => {
-  setAILearning(true)
-  try {
-    const response_0 = await fetch(images[0]);
-    const exampleImage = await response_0.blob();
+
+
+// export const askAQuestion = async (images, setAILearning,title,creator) => {
+//   setAILearning(true)
+//   try {
+//     const response_0 = await fetch(images[0]);
+//     const exampleImage = await response_0.blob();
     						
-const app = await client("https://llava.hliu.cc/");
-const result = await app.predict("Extract the human readable text from the image", [		
-				"Hello!!", // string  in 'parameter_3' Textbox component
-				exampleImage, 	// blob in 'parameter_11' Image component		
-				"default", // string  in 'Preprocess for non-square image' Radio component
-	]);
-  setAILearning(false)
-  console.log(result.data);
-  return result;
-  } catch (error) {
-    throw error
-  }
+// const app = await client("https://llava.hliu.cc/");
+// const result = await app.predict("Extract the human readable text from the image", [		
+// 				"Hello!!", // string  in 'parameter_3' Textbox component
+// 				exampleImage, 	// blob in 'parameter_11' Image component		
+// 				"default", // string  in 'Preprocess for non-square image' Radio component
+// 	]);
+//   setAILearning(false)
+//   console.log(result.data);
+//   return result;
+//   } catch (error) {
+//     throw error
+//   }
  
  
-};
+// };
 
 
   // export const askAQuestion = async (images, setAILearning,title,creator) => {
@@ -32,7 +33,7 @@ const result = await app.predict("Extract the human readable text from the image
   //     `${baseUrl}/langchain/askAQuestion`,
   //     JSON.stringify({ images })
   //   );
-    
+  //   console.log('--ai-response--',response);
 
   //   if (response.error) {
   //     console.log("error", response);
--- server/src/controllers/langchainController.js
+++ server/src/controllers/langchainController.js
@@ -6,29 +6,51 @@ const fetch = require("node-fetch");
 
 
 
+
 router.post("/askAQuestion", async (req, res) => {
-  const imageUrls = req.body.images;
-  console.log(imageUrls);
+  const images= req.body.images
+try {
+  const response_0 = await fetch(images[0]);
+  const exampleImage = await response_0.blob();
+
+  const app = await client("https://llava.hliu.cc/");
+  const result = await app.predict(
+    "Extract the human readable text from the image",
+    [
+      "Hello!!", // string  in 'parameter_3' Textbox component
+      exampleImage, // blob in 'parameter_11' Image component
+      "default", // string  in 'Preprocess for non-square image' Radio component
+    ]
+  );
+  res.json({ result });
+} catch (error) {
+  console.log(error);
+}
+    
+});
+// router.post("/askAQuestion", async (req, res) => {
+//   const imageUrls = req.body.images;
+//   console.log(imageUrls);
  
-  const allDataFromImages = [];
-  const worker = await createWorker('eng');
-    for (const url of imageUrls){
-      try {
+//   const allDataFromImages = [];
+//   const worker = await createWorker('eng');
+//     for (const url of imageUrls){
+//       try {
 
        
-        const ret = await worker.recognize(url);
-        console.log(ret.data.text);
-        allDataFromImages.push(ret.data.text)
+//         const ret = await worker.recognize(url);
+//         console.log(ret.data.text);
+//         allDataFromImages.push(ret.data.text)
        
       
-        } catch (error) {
-          console.error("Error during text recognition:", error);
-          res.status(500).json({ error: error.message });
-        }
-    } 
-    await worker.terminate();
-    res.json({ allDataFromImages });
-});
+//         } catch (error) {
+//           console.error("Error during text recognition:", error);
+//           res.status(500).json({ error: error.message });
+//         }
+//     } 
+//     await worker.terminate();
+//     res.json({ allDataFromImages });
+// });
 
 
 
